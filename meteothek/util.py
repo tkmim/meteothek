c461bd7ec83dc0dc73b1253c0bf7848357f6d759
Takumi Matsunobu <tkmim@users.noreply.github.com>
2020-10-02 11:20:16 +0200

    Added the function to make a nonlinear colour map

--- meteothek/util.py
+++ meteothek/util.py
@@ -3,6 +3,7 @@
 """
 
 import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
 
 
 def date_format(yyyy,mm,dd,hh):
@@ -10,6 +11,7 @@ def date_format(yyyy,mm,dd,hh):
     return str('%04d%02d%02d%02d%02d' % yyyy, mm, dd, hh)
 
 def mkcmap(colors, levels):
+    """Make a color map from listed colours"""
     from matplotlib.colors import ListedColormap, BoundaryNorm
 
     #cm = LinearSegmentedColormap.from_list('custom_cmap', list_cid, N=len(list_cid))
@@ -18,3 +20,19 @@ def mkcmap(colors, levels):
 
     return cm, norm
 
+
+class nlcmap(LinearSegmentedColormap):
+    """A nonlinear colormap from given Colormap"""
+    name = 'nlcmap'
+
+    def __init__(self, cmap, levels):
+        self.cmap = cmap
+        self.monochrome = self.cmap.monochrome
+        self.levels = np.asarray(levels, dtype='float64')
+        self._x = self.levels-self.levels.min()
+        self._x/= self._x.max()
+        self._y = np.linspace(0, 1, len(self.levels))
+
+    def __call__(self, xi, alpha=1.0, **kw):
+        yi = np.interp(xi, self._x, self._y)
+        return self.cmap(yi, alpha)
