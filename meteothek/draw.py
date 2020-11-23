73a2083c2e3f264e4879ac3286a374fdba02ed07
Takumi Matsunobu <tkmim@users.noreply.github.com>
2020-11-24 00:05:07 +0100

    Bug fix

--- meteothek/draw.py
+++ meteothek/draw.py
@@ -98,18 +98,28 @@ def mesh(map, lons, lats, data, *, cmap=SCM6.batlow, **kwargs):
     return mesh
 
 
-def contour(map, lons, lats, data, *, levels=-9999, colors="black", linestyles='-',linewidths=0.5):
+def contour(map, lons, lats, data, *, levels=-9999, colors="black", linestyles='-',linewidths=0.5, **kwargs):
 
+    print('Keywords attached: ',*kwargs)
+    kwargs.setdefault("projection", map.projection)
+    
     if levels == -9999:
         levels=np.linspace(np.min(data), np.max(data), 6)
 
-    contour = map.contour(lons, lats, data, colors=colors, linestyles=linestyles, linewidths=linewidths, levels=levels)
+    contour = map.contour(lons, lats, data, colors=colors, linestyles=linestyles, linewidths=linewidths, levels=levels,
+                         transform=projection)
+    
     contour.clabel(fmt='%1.1f', fontsize=8)
 
     return contour
 
 
-def vector(map, lons, lats, u, v, *, skip=10, scale=1.0, legend=True ):
+def vector(map, lons, lats, u, v, *, skip=10, scale=1.0, legend=True, **kwargs ):
+
+
+    print('Keywords attached: ',*kwargs)
+    kwargs.setdefault("projection", map.projection)
+    
     scale=scale * 0.0393701
     # avoid zero division
     if scale == 0:
@@ -117,7 +127,7 @@ def vector(map, lons, lats, u, v, *, skip=10, scale=1.0, legend=True ):
 
     arr_skip=(slice(None,None,skip), slice(None,None,skip))
     vector = map.quiver(lons[arr_skip], lats[arr_skip], u[arr_skip], v[arr_skip],
-                        angles='xy', scale_units='inches', scale=1/scale)
+                        angles='xy', scale_units='inches', scale=1/scale, transform=projection)
     
     if(legend):
         plt.gca().quiverkey(vector, 0.9, 1.05, U=5, label='5 m/s', 
@@ -128,9 +138,12 @@ def barb(map,x,y,u,v):
     pass
 
 def point(map, lons, lats, *, marker="o", markersize=10, color='blue'):
-    map.plot(lons, lats, marker=marker, markersize=markersize, color=color)
+    """ Plot a point on a map """
+    
+    map.plot(lons, lats, marker=marker, markersize=markersize, color=color, transform=ccrs.Geodetic()._as_mpl_transform(map))
 
 def text(map, lons, lats, string, *, size=20, color="black"):
+    """ Draw text on a map """
     
     map.text(lons, lats, string, size=size, color=color,
             transform=ccrs.Geodetic()._as_mpl_transform(map))
@@ -139,9 +152,10 @@ def title(map, txt, *, loc='left', fontsize=10):
     plt.title(txt, loc=loc, fontsize=fontsize)
 
 def colorbar(fig, cs, position):
-        cax=fig.add_subplot(1,1,1, label='cax')
-        cax_pos = [position[0], position[2], 
-                position[1]-position[0], position[3]-position[2]]
-        cax.set_position(cax_pos)
-        fig.colorbar(cs, cax=cax)
+    
+    cax=fig.add_subplot(1,1,1, label='cax')
+    cax_pos = [position[0], position[2], 
+        position[1]-position[0], position[3]-position[2]]
+    cax.set_position(cax_pos)
+    fig.colorbar(cs, cax=cax)
 
