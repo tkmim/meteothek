a6d3316d2a13988bb4ba30ad149fa606625922da
Takumi Matsunobu <tkmim@users.noreply.github.com>
2021-10-27 14:54:52 +0200

    Add new features in util and Refactor draw

--- meteothek/draw.py
+++ meteothek/draw.py
@@ -2,7 +2,7 @@
 """
 
 """
-
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
@@ -14,14 +14,15 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from cartopy.mpl.ticker import LatitudeFormatter,LongitudeFormatter
 
-def coastline(map, *, coast_lw=0.5, border_lw=0.3 ):
+
+def coastline(ax, *, coast_lw=0.5, border_lw=0.3 ):
     
     # draw coastlines 
-    map.add_feature(cfeature.COASTLINE, edgecolor='black', linewidth=coast_lw)
-    map.add_feature(cfeature.BORDERS, linewidth=border_lw)
+    ax.add_feature(cfeature.COASTLINE, edgecolor='black', linewidth=coast_lw)
+    ax.add_feature(cfeature.BORDERS, linewidth=border_lw)
 
     
-def gridlines(map, *, label=True, latint=15, lonint=15):
+def gridlines(ax, *, label=True, latint=15, lonint=15, linewidth=1.0):
 
 
     # define gridline intervals
@@ -36,123 +37,175 @@ def gridlines(map, *, label=True, latint=15, lonint=15):
     latfmt = LatitudeFormatter(number_format='.1f',
                                           degree_symbol='')
     
-    gl = map.gridlines(draw_labels=label,
+    gl = ax.gridlines(draw_labels=label,
                 xlocs=xticks, ylocs=yticks,
                 xformatter=lonfmt, yformatter=latfmt,
+                linewidth=linewidth,
                 x_inline=False, y_inline=False)
     
     gl.top_labels = gl.right_labels = False
         
 
-def shade(map, lons, lats, data, *, cmap=SCM6.batlow, extend='both', **kwargs):
+def shade(ax, lons, lats, data, *, cmap=SCM6.batlow, extend='both', **kwargs):
 
     print('draw.shade: keywords attached: ',*kwargs)
-    kwargs.setdefault("projection", map.projection)
+    kwargs.setdefault("projection", ax.projection)
+    projection = kwargs.get('projection', False)
     
     # Unfold kwargs
-    levels = kwargs.get('levels', False) 
-    colors = kwargs.get('colors', False)
-    projection = kwargs.get('projection', False)
-
-    if colors != False:
-        cmap = util.mkcmap(colors)
-
-    if levels != False:
-        cmap = util.nlcmap(cmap, levels)
-
-    if levels != False:
-        shade = map.contourf(lons, lats, data, levels, cmap=cmap, extend=extend, transform=projection)
-
-    else:
-        shade = map.contourf(lons, lats, data, cmap=cmap, extend=extend, transform=projection)
+    keywords = {}
+    if 'scale' in kwargs and kwargs['scale'] == 'log':
+        keywords['locator']=mticker.LogLocator()
+    if 'colors' in kwargs:
+        cmap = util.mkcmap(kwargs['colors'])
+        if not 'levels' in kwargs or len(kwargs['levels']) != len(kwargs['colors']) + 1:
+            print('Error: optional keyword "colors" must be used with "levels" and their lengths must be n and n+1')
+            sys.exit()
+            
+    if 'levels' in kwargs:
+        keywords['levels']=kwargs['levels']
+        cmap = util.nlcmap(cmap, keywords['levels'])
+        
+    shade = ax.contourf(lons, lats, data, cmap=cmap, extend=extend, transform=projection, **keywords)
 
     return shade
 
 
-def mesh(map, lons, lats, data, *, cmap=SCM6.batlow, **kwargs):
+def mesh(ax, lons, lats, data, *, cmap=SCM6.batlow, **kwargs):
     
-    print('Keywords attached: ',*kwargs)
-    kwargs.setdefault("projection", map.projection)
-    
-    # Unfold kwargs
-    #colors = kwargs.get('colors', False)
-    #levels = kwargs.get('levels', False) 
+    print('draw.mesh: keywords attached: ',*kwargs)
+    kwargs.setdefault("projection", ax.projection)
     projection = kwargs.get('projection', False)
     
-    # User specified discontinuous colourbar is omitted.
-    # Use draw.shade for it
-    #if colors != False:
-    #    cmap = util.mkcmap(colors)
-    #    
-    #if levels != False:
-    #    cmap = util.nlcmap(cmap, levels)        
-
-    mesh = map.pcolormesh(lons, lats, data, cmap=cmap, transform=projection)
+    # Unfold kwargs
+    keywords = {}
+    if 'scale' in kwargs and kwargs['scale'] == 'log':
+        keywords['locator']=mticker.LogLocator()        
+    if 'colors' in kwargs:
+        cmap = util.mkcmap(kwargs['colors'])
+        if not 'levels' in kwargs or len(kwargs['levels']) != len(kwargs['colors']) + 1:
+            print('Error: optional keyword "colors" must be used with "levels" and their lengths must be n and n+1')
+            sys.exit()    
+    if 'levels' in kwargs:
+        import matplotlib.colors
+        keywords['norm'] = matplotlib.colors.BoundaryNorm(kwargs['levels'], cmap.N) 
+        
+    mesh = ax.pcolormesh(lons, lats, data, cmap=cmap, transform=projection, **keywords)
 
     return mesh
 
 
-def contour(map, lons, lats, data, *, levels=False, colors="black", linestyles='-',linewidths=0.5, **kwargs):
-
-    print('draw.contour: Keywords attached: ',*kwargs)
-    kwargs.setdefault("projection", map.projection)
+def contour(ax, lons, lats, data, *, colors="black", 
+            labels=True, linestyles='-',linewidth=0.5, cfmt='%1.1f', **kwargs):
     
-    # Unfold kwargs
-    projection = kwargs.get('projection', False)    
-
-    if type(levels) is bool and levels == False:
-        levels=np.linspace(np.min(data), np.max(data), 6)
-
-    contour = map.contour(lons, lats, data, colors=colors, linestyles=linestyles, linewidths=linewidths, levels=levels,
-                         transform=projection)
+    print('draw.contour: keywords attached: ',*kwargs)
+    kwargs.setdefault("projection", ax.projection)
+    projection = kwargs.get('projection', False)  
     
-    contour.clabel(fmt='%1.1f', fontsize=8)
+    # Unfold kwargs  
+    keywords = {}
+    if 'levels' in kwargs:
+        keywords['levels'] = kwargs['levels']
+    else:
+        keywords['levels'] = levels=np.linspace(np.min(data), np.max(data), 6)
+        
+    contour = ax.contour(lons, lats, data, colors=colors, linestyles=linestyles, linewidths=linewidth,
+                         transform=projection, **keywords)
+    
+    if labels == True:
+        contour.clabel(fmt=cfmt, fontsize=8)
 
     return contour
 
 
-def vector(map, lons, lats, u, v, *, skip=10, scale=1.0, legend=True, **kwargs ):
-
+def vector(ax, lons, lats, u, v, *, skip=10, scale=1.0, legend=True, U=5, labelunit='m/s', colors='black', **kwargs ):
+    """
+    
+    param: scale: arrow size scale factor (e.g. scale=1.0, wind speed 10.0 m/s is depicted as 1 cm)
+    """
 
     print('Keywords attached: ',*kwargs)
-    kwargs.setdefault("projection", map.projection)
+    kwargs.setdefault("projection", ax.projection)
+    projection = kwargs.get('projection', False)   
+
+    # Unfold kwargs
+    
     
     scale=scale * 0.0393701
     # avoid zero division
     if scale == 0:
-        scale = 0.0393701
-
+        print('Error: "scale" must not be zero')
+        sys.exit() 
+        
     arr_skip=(slice(None,None,skip), slice(None,None,skip))
-    vector = map.quiver(lons[arr_skip], lats[arr_skip], u[arr_skip], v[arr_skip],
-                        angles='xy', scale_units='inches', scale=1/scale, transform=projection)
+    
+    vector = ax.quiver(lons[arr_skip], lats[arr_skip], u[arr_skip], v[arr_skip],
+                        angles='uv', scale_units='inches', scale=1/scale,
+                        color=colors, zorder=5, transform=projection)
     
     if(legend):
-        plt.gca().quiverkey(vector, 0.9, 1.05, U=5, label='5 m/s', 
+        plt.gca().quiverkey(vector, 0.9, 1.05, U=U, label='%s %s' % (U, labelunit), 
                 labelpos='E', coordinates='axes')
+
     return vector
 
-def barb(map,x,y,u,v):
+def barb(ax,x,y,u,v):
     pass
 
-def point(map, lons, lats, *, marker="o", markersize=10, color='blue'):
+def point(ax, lons, lats, *, marker="o", markersize=10, colors='blue'):
     """ Plot a point on a map """
     
-    map.plot(lons, lats, marker=marker, markersize=markersize, color=color, transform=ccrs.Geodetic()._as_mpl_transform(map))
+    ax.plot(lons, lats, marker=marker, markersize=markersize, color=colors, 
+            transform=ccrs.Geodetic()._as_mpl_transform(ax))
 
-def text(map, lons, lats, string, *, size=20, color="black"):
+def text(ax, lons, lats, string, *, size=20, colors="black"):
     """ Draw text on a map """
     
-    map.text(lons, lats, string, size=size, color=color,
-            transform=ccrs.Geodetic()._as_mpl_transform(map))
+    ax.text(lons, lats, string, size=size, color=colors,
+            transform=ccrs .Geodetic()._as_mpl_transform(ax))
 
-def title(map, txt, *, loc='left', fontsize=10):
+def title(ax, txt, *, loc='left', fontsize=10):
     plt.title(txt, loc=loc, fontsize=fontsize)
 
-def colorbar(fig, cs, position):
     
+def colorbar(fig, cs, position, orientation='vertical', ticks=False):
+    """
+    param: position: [ left, right, bottom, top] #values between 0.0-1.0
+    """
+    keywords = {}
+    if ticks is not False:
+        keywords['ticks'] = ticks
+         
     cax=fig.add_subplot(1,1,1, label='cax')
     cax_pos = [position[0], position[2], 
         position[1]-position[0], position[3]-position[2]]
     cax.set_position(cax_pos)
-    fig.colorbar(cs, cax=cax)
+    fig.colorbar(cs, cax=cax, orientation=orientation, **keywords)
 
+    
+def add_subplot_axes(ax, rect, *, facecolor='w'):
+    """
+    param: rect[]
+    """
+    fig = plt.gcf()
+    box = ax.get_position()
+    width = box.width
+    height = box.height
+    
+    inax_position  = ax.transAxes.transform(rect[0:2])
+    transFigure = fig.transFigure.inverted()
+    infig_position = transFigure.transform(inax_position)    
+    x = infig_position[0]
+    y = infig_position[1]
+    width *= rect[2]
+    height *= rect[3]
+    subax = fig.add_axes([x,y,width,height],facecolor=facecolor)  # matplotlib 2.0+
+    
+    x_labelsize = subax.get_xticklabels()[0].get_size()
+    y_labelsize = subax.get_yticklabels()[0].get_size()
+    x_labelsize *= rect[2]**0.5
+    y_labelsize *= rect[3]**0.5
+    subax.xaxis.set_tick_params(labelsize=x_labelsize)
+    subax.yaxis.set_tick_params(labelsize=y_labelsize)
+
+    return subax
--- meteothek/util.py
+++ meteothek/util.py
@@ -4,10 +4,11 @@
 
 import numpy as np
 import time
-from matplotlib.colors import LinearSegmentedColormap
-
+from scipy import signal
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 
 class timer(object):
+    """ Timer """
     def __init__(self, name, verbose=True):
         self.verbose = verbose
         self.name = name
@@ -25,26 +26,29 @@ class timer(object):
         elif self.verbose :
             print(self.name, ': elapsed time: %.3f ms' % self.msecs)
 
+def flush():
+    """Flush standard outputs to an external file"""
+    import sys
+    sys.stdout.flush()
 
-def date_format(yyyy,mm,dd,hh):
+def date_to_ymdh(yyyy,mm,dd,hh):
     """Format date to YYYYMMDDHH"""
     return str('%04d%02d%02d%02d%02d' % yyyy, mm, dd, hh)
 
 
 def mkcmap(colors):
     """Make a color map from listed colours"""
-    from matplotlib.colors import ListedColormap, BoundaryNorm
-
-    #cm = LinearSegmentedColormap.from_list('custom_cmap', list_cid, N=len(list_cid))
-    
     cmap = ListedColormap(colors, name="custom")        
-    #norm = BoundaryNorm(levels, len(levels))
 
     return cmap
 
 
 class nlcmap(LinearSegmentedColormap):
-    """A nonlinear colormap from given Colormap"""
+    """
+    Make a nonlinear colormap from given Colormap
+    
+    Usage: nlcmap = util.nlcmap(cmap, levels)
+    """
     name = 'nlcmap'
 
     def __init__(self, cmap, levels):
@@ -58,3 +62,36 @@ class nlcmap(LinearSegmentedColormap):
     def __call__(self, xi, alpha=1.0, **kw):
         yi = np.interp(xi, self._x, self._y)
         return self.cmap(yi, alpha)
+
+    
+def fss(fcst, obs, threshold, window, thrsd_type):
+    """
+    Compute the fraction skill score using convolution.
+    :param fcst: nd-array, forecast field.
+    :param obs: nd-array, observation field.
+    :param window: integer, window size.
+    :param thrsd_type: character, "accumulation" or "percentile"
+    
+    :return: tuple of FSS numerator, denominator and score.
+    """
+    def fourier_filter(field, n):
+        return signal.fftconvolve(field, np.ones((n, n)))
+
+    # Obtain the thresholds
+    if thrsd_type == 'accumulation':
+        thresh_fx = threshold
+        thresh_obs = threshold
+    elif thrsd_type == 'percentile':  # ~ 'percentile'
+        thresh_fx = np.nanpercentile(fcst, threshold)
+        thresh_obs = np.nanpercentile(obs, threshold)
+    else:
+        exit("Invalid FSS threshold type: select \"accumulation\" or \"percentile\" ")
+
+    fhat = fourier_filter(fcst > thresh_fx, window)
+    ohat = fourier_filter(obs > thresh_obs, window)
+
+    num = np.nanmean(np.power(fhat - ohat, 2))
+    denom = np.nanmean(np.power(fhat, 2) + np.power(ohat, 2))
+
+    return num, denom, 1. - num / denom
+    
